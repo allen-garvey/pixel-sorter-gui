0c6f53e9f3845059d833f43e0a3e847405c9cb6c
Allen Garvey <allen.garvey@gmail.com>
2017-03-16 15:24:16 -0400

    Edit pixel sorter function to enable sorting by different colors

--- pixelsorter.cpp
+++ pixelsorter.cpp
@@ -8,14 +8,23 @@ void PixelSorter::pixelSortHorizontal(QImage *image, PixelSorterColor sortType, 
     //sanity check on endIndex
     endIndex = endIndex > imageHeight ? imageHeight : endIndex;
 
+    auto sortFunc = [&](QRgb const & a, QRgb const & b) -> bool {
+        if(sortType == red){
+            return qRed(a) < qRed(b);
+        }
+        else if(sortType == green){
+            return qGreen(a) < qGreen(b);
+        }
+        return qBlue(a) < qBlue(b);
+    };
+
     //pixel manipulation based on: http://stackoverflow.com/questions/2095039/qt-qimage-pixel-manipulation
     int linesSorted = 0;
     QRgb *rowData;
     for(int i=startIndex;i<endIndex;i++){
         rowData = (QRgb*)image->scanLine(i);
         std::sort(rowData, rowData + imageWidth - 1,
-                  [](QRgb const & a, QRgb const & b) -> bool
-                  { return qRed(a) < qRed(b); } );
+                  sortFunc );
         linesSorted++;
         if(linesSorted == countIndex){
             linesSorted = 0;
