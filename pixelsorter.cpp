d395777e5417d9b2c8e5c02a3618ea918cd2f911
Allen Garvey <allen.garvey@gmail.com>
2017-03-16 14:53:38 -0400

    Edit pixel sorting function to use countIndex and skipIndex parameters

--- pixelsorter.cpp
+++ pixelsorter.cpp
@@ -7,12 +7,19 @@ void PixelSorter::pixelSortHorizontal(QImage *image, PixelSorterColor sortType, 
     int imageWidth = image->width();
     //sanity check on endIndex
     endIndex = endIndex > imageHeight ? imageHeight : endIndex;
+
     //pixel manipulation based on: http://stackoverflow.com/questions/2095039/qt-qimage-pixel-manipulation
+    int linesSorted = 0;
     QRgb *rowData;
     for(int i=startIndex;i<endIndex;i++){
         rowData = (QRgb*)image->scanLine(i);
         std::sort(rowData, rowData + imageWidth - 1,
                   [](QRgb const & a, QRgb const & b) -> bool
                   { return qRed(a) < qRed(b); } );
+        linesSorted++;
+        if(linesSorted == countIndex){
+            linesSorted = 0;
+            i += skipIndex;
+        }
     }
 }
